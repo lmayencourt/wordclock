a1581a2811456fa36233de5fca357ddf6e54d222
Louis Mayencourt <louis.mayencourt@gmail.com>
2022-01-30 20:25:11 +0100

    Launch test with dlb after a sucessfull build
    Signed-off-by: Louis Mayencourt <louis.mayencourt@gmail.com>

--- build-description/build.py
+++ build-description/build.py
@@ -5,27 +5,43 @@ import dlb_contrib.gcc
 class CCompiler(dlb_contrib.gcc.CCompilerGcc):
     DEFINITIONS = {'UNITY_FIXTURE_NO_EXTRAS':1}
 
+class UnitTest(dlb.ex.Tool):
+    EXECUTABLE = 'unity_test'
+
+    async def redo(self, result, context):
+        # context.helper['unity_test/'] =  'dist/test/'
+        await context.execute_helper(
+            self.EXECUTABLE,
+            cwd='dist/test/'
+        )
+
 # compile and link application written in C
 with dlb.ex.Context():
     source_directory = dlb.fs.Path('test/')
     unity_include_directory = dlb.fs.Path('test/unity/')
-    output_directory = dlb.fs.Path('test/build/')
+    build_output_directory = dlb.fs.Path('build/')
+    distribution_directory = dlb.fs.Path('dist/test/')
 
-    with dlb.di.Cluster('compile'), dlb.ex.Context(max_parallel_redo_count=4):
+    with dlb.di.Cluster('Compile'), dlb.ex.Context(max_parallel_redo_count=4):
         compile_results = [
             CCompiler(
                 source_files=[p],
-                object_files=[output_directory / p.with_appended_suffix('.o')],
+                object_files=[build_output_directory / p.with_appended_suffix('.o')],
                 include_search_directories=[source_directory, unity_include_directory],
 
             ).start()
-            for p in source_directory.iterdir(name_filter=r'.+\.c', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
+            for p in source_directory.iterdir(name_filter=r'.+\.(?:c|cpp)', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
         ]
 
-    with dlb.di.Cluster('link'), dlb.ex.Context():
+    with dlb.di.Cluster('Link'), dlb.ex.Context():
         object_files = [r.object_files[0] for r in compile_results]
         dlb_contrib.gcc.CLinkerGcc(
             object_and_archive_files=object_files,
-            linked_file=output_directory / 'unity_test').start()
+            linked_file=distribution_directory / 'unity_test').start()
+
+    with dlb.di.Cluster('Test'), dlb.ex.Context():
+        # TODO: Check how to do define a tool form a generated build-product
+        dlb.ex.Context.active.helper['unity_test'] = '/Users/louismayencourt/project/wordclock_upstream/dist/test/unity_test'
+        UnitTest().start()
 
 dlb.di.inform('finished successfully')
