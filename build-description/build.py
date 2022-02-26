32822620e81506ccf315f5e47b559ea64740c7c7
Louis Mayencourt <louis.mayencourt@gmail.com>
2022-02-26 14:53:52 +0100

    Make linker almost happy
    Signed-off-by: Louis Mayencourt <louis.mayencourt@gmail.com>

--- build-description/build.py
+++ build-description/build.py
@@ -34,6 +34,49 @@ class CppXtensaEsp32Compiler(dlb_contrib.gcc._CompilerGcc):
         compile_arguments.extend(['-Os', '-g3', '-Wpointer-arith', '-fexceptions', '-fstack-protector', '-ffunction-sections', '-fdata-sections', '-fstrict-volatile-bitfields', '-mlongcalls', '-nostdlib', '-w', '-Wno-error=maybe-uninitialized', '-Wno-error=unused-function', '-Wno-error=unused-but-set-variable', '-Wno-error=unused-variable', '-Wno-error=deprecated-declarations', '-Wno-unused-parameter', '-Wno-unused-but-set-parameter', '-Wno-missing-field-initializers', '-Wno-sign-compare', '-fno-rtti', '-MMD', '-c'])
         return compile_arguments
 
+class CppXtensaEsp32Linker(dlb_contrib.gcc._LinkerGcc):
+    EXECUTABLE = 'xtensa-esp32-elf-gcc'
+
+    LIBRARY_FILENAMES = ('gcc', 'esp_websocket_client', 'wpa2', 'detection', 'esp_https_server', 'wps', 'hal', 'console', 'pe', 'soc', 'sdmmc', 'pthread', 'log', 'esp_http_client', 'json', 'mesh', 'esp32-camera', 'net80211', 'wpa_supplicant', 'c', 'mqtt', 'cxx', 'esp_https_ota', 'ulp', 'efuse', 'pp', 'mdns', 'bt', 'wpa', 'spiffs', 'heap', 'image_util', 'unity', 'rtc', 'mbedtls', 'face_recognition', 'nghttp', 'jsmn', 'openssl', 'core', 'fatfs', 'm', 'protocomm', 'smartconfig', 'xtensa-debug-module', 'dl', 'esp_event', 'esp-tls', 'fd', 'espcoredump', 'esp_http_server', 'fr', 'smartconfig_ack', 'wear_levelling', 'tcp_transport', 'lwip', 'phy', 'vfs', 'coap', 'esp32', 'libsodium', 'bootloader_support', 'driver', 'coexist', 'asio', 'od', 'micro-ecc', 'esp_ringbuf', 'detection_cat_face', 'app_update', 'espnow', 'face_detection', 'app_trace', 'newlib', 'btdm_app', 'wifi_provisioning', 'freertos', 'freemodbus', 'ethernet', 'nvs_flash', 'spi_flash', 'c_nano', 'expat', 'fb_gfx', 'protobuf-c', 'esp_adc_cal', 'tcpip_adapter ', 'stdc++')
+
+    def get_all_link_arguments(self) -> List[Union[str, dlb.fs.Path, dlb.fs.Path.Native]]:
+        link_arguments = ['-nostdlib']
+        link_arguments += self.get_search_link_arguments()
+        link_arguments += self.get_extra_link_arguments()
+        return link_arguments
+
+    def get_extra_link_arguments(self) -> List[Union[str, dlb.fs.Path, dlb.fs.Path.Native]]:
+        link_arguments = ['-Tesp32_out.ld', '-Tesp32.project.ld', '-Tesp32.rom.ld', '-Tesp32.peripherals.ld', '-Tesp32.rom.libgcc.ld', '-Tesp32.rom.spiram_incompatible_fns.ld', '-u esp_app_desc', '-u ld_include_panic_highint_hdl', '-u call_user_start_cpu0', '-Wl,--gc-sections', '-Wl,-static', '-Wl,--undefined=uxTopUsedPriority', '-u __cxa_guard_dummy', '-u __cxx_fatal_exception']
+        return link_arguments
+
+    def get_search_link_arguments(self) -> List[Union[str, dlb.fs.Path]]:
+            link_arguments = []
+            if self.library_search_directories:
+                for p in self.library_search_directories:
+                    link_arguments.extend(['-L' + p.as_string()])  # looked up for -lxxx
+            return link_arguments
+
+    def get_subprogram_link_arguments(self, context) -> List[Union[str, dlb.fs.Path]]:
+        return []
+
+    async def redo(self, result, context):
+        link_arguments = self.get_all_link_arguments()
+        link_arguments += self.get_subprogram_link_arguments(context)
+
+        # link
+        with context.temporary() as linked_file:
+            link_arguments += [
+                '-o', linked_file,
+                *result.object_and_archive_files  # note: type detection by suffix of path cannot be disabled
+            ]
+
+            # https://linux.die.net/man/1/ld
+            for lib in self.LIBRARY_FILENAMES:
+                link_arguments += ['-l' + lib]  # if l is empty: '/usr/bin/ld: cannot find -l:'
+
+            await context.execute_helper(self.EXECUTABLE, link_arguments)
+            context.replace_output(result.linked_file, linked_file)
+
 class CppCompiler(dlb_contrib.gcc.CplusplusCompilerGcc):
     DEFINITIONS = {'UNITY_FIXTURE_NO_EXTRAS':1}
 
@@ -62,6 +105,7 @@ with dlb.ex.Context():
     firmware_include_directory = dlb.fs.Path('firmware/include/')
 
     arduino_esp32_directory = dlb.fs.Path('external_dependencies/arduino-esp32/')
+    arduino_esp32_absolute_directory = dlb.ex.Context.active.root_path / arduino_esp32_directory
     arduino_esp32_core_source_directory = arduino_esp32_directory / 'cores/esp32/'
     # arduino_esp32_wifi_library = arduino_esp32_directory / 'libraries/Ethernet/'
     # arduino_esp32_wifi_library = arduino_esp32_directory / 'libraries/Ethernet/src/'
@@ -96,6 +140,8 @@ with dlb.ex.Context():
     for dir in arduino_esp32_core_include_list:
         arduino_esp32_sdk_include_directory.append(arduino_esp32_directory / dir)
 
+    adafruit_neopixel_source_directory = dlb.fs.Path('/Users/louismayencourt/Documents/Arduino/libraries/Adafruit_NeoPixel/')
+
     test_source_directory = dlb.fs.Path('test/')
     test_spy_sources_directory = dlb.fs.Path('test/spy/')
     unity_include_directory = dlb.fs.Path('test/unity/')
@@ -111,25 +157,35 @@ with dlb.ex.Context():
 
         with dlb.di.Cluster('Compile Arduino C Core files'), dlb.ex.Context(max_parallel_redo_count=parallel_build_redo):
             arduino_esp32_core_c_compile_results = [
-                        CXtensaEsp32Compiler(
-                            source_files=[p],
-                            object_files=[build_output_directory / p.with_appended_suffix('.o')],
-                            include_search_directories= arduino_esp32_sdk_include_directory,
-                        ).start()
-                        for p in arduino_esp32_core_source_directory.iterdir(name_filter=r'.+\.c', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
-                    ]
+                    CXtensaEsp32Compiler(
+                        source_files=[p],
+                        object_files=[build_output_directory / p.with_appended_suffix('.o')],
+                        include_search_directories= arduino_esp32_sdk_include_directory,
+                    ).start()
+                    for p in arduino_esp32_core_source_directory.iterdir(name_filter=r'.+\.c', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
+                ]
 
         with dlb.di.Cluster('Compile Arduino Cpp Core library'), dlb.ex.Context(max_parallel_redo_count=parallel_build_redo):
             arduino_esp32_core_cpp_compile_results = [
-                        CppXtensaEsp32Compiler(
-                            source_files=[p],
-                            object_files=[build_output_directory / p.with_appended_suffix('.o')],
-                            include_search_directories=arduino_esp32_sdk_include_directory,
-                        ).start()
-                        for p in arduino_esp32_core_source_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
-                    ]
+                    CppXtensaEsp32Compiler(
+                        source_files=[p],
+                        object_files=[build_output_directory / p.with_appended_suffix('.o')],
+                        include_search_directories=arduino_esp32_sdk_include_directory,
+                    ).start()
+                    for p in arduino_esp32_core_source_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
+                ]
 
-        with dlb.di.Cluster('Compile Firmware hpp'), dlb.ex.Context():
+        with dlb.di.Cluster('Compile external libraries'), dlb.ex.Context():
+            adafruit_neopixel_compile_results = [
+                    CppXtensaEsp32Compiler(
+                        source_files=[p],
+                        object_files=[build_output_directory / p.with_appended_suffix('.o')],
+                        include_search_directories=arduino_esp32_sdk_include_directory,
+                    ).start()
+                    for p in adafruit_neopixel_source_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False, recurse_name_filter=lambda n: '.' not in n)
+                ]
+
+        with dlb.di.Cluster('Compile Firmware hpp'), dlb.ex.Context(max_parallel_redo_count=parallel_build_redo):
             firmware_hpp_source_directory = dlb.fs.Path('firmware/')
             firmware_hpp_include_directory = [firmware_include_directory]
             firmware_hpp_include_directory.extend(arduino_esp32_sdk_include_directory)
@@ -147,14 +203,27 @@ with dlb.ex.Context():
                 for p in firmware_hpp_source_directory.iterdir(name_filter=r'.+\.hpp', is_dir=False)
             ]
 
-        firmware_compile_results = [
-            CppXtensaEsp32Compiler(
-                source_files=[p],
-                object_files=[build_output_directory / p.with_appended_suffix('.o')],
-                include_search_directories=[firmware_include_directory],
-            ).start()
-            for p in firmware_source_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False)
-        ]
+        with dlb.di.Cluster('Link Firmware'), dlb.ex.Context():
+            dlb.cf.level.helper_execution = dlb.di.INFO
+            object_files = [r.object_files[0] for r in arduino_esp32_core_c_compile_results]
+            object_files += [r.object_files[0] for r in arduino_esp32_core_cpp_compile_results]
+            object_files += [r.object_files[0] for r in adafruit_neopixel_compile_results]
+            object_files += (r.object_files[0] for r in firmware_hpp_compile_results)
+            test_binary = CppXtensaEsp32Linker(
+                object_and_archive_files=object_files,
+                library_search_directories=[arduino_esp32_absolute_directory / 'tools/sdk/ld/',
+                                            arduino_esp32_absolute_directory / 'tools/sdk/lib/'],
+                linked_file=distribution_directory / 'firmware'
+                ).start().linked_file
+
+        # firmware_compile_results = [
+        #     CppXtensaEsp32Compiler(
+        #         source_files=[p],
+        #         object_files=[build_output_directory / p.with_appended_suffix('.o')],
+        #         include_search_directories=[firmware_include_directory],
+        #     ).start()
+        #     for p in firmware_source_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False)
+        # ]
 
     with dlb.di.Cluster('Compile tests'), dlb.ex.Context():
         firmware_compile_results = [
