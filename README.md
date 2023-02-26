a9401bd5b03e57ab968279162a94631f54c58f3b
Louis Mayencourt <louis.mayencourt@gmail.com>
2023-02-26 10:12:44 +0100

    Add LED blink

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ cargo build
 espflash /dev/tty.usbserial-2110 target/xtensa-esp32-espidf/debug/hello-world
 espmonitor /dev/tty.usbserial-2110
 ````
+
+## Setup rust analyzer for ESP32
+setup the environment variables in `.cargo/config.toml` to match the `export-esp.sh` values.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::thread;
+use std::time::Duration;
+
+use embedded_hal::digital::v2::OutputPin;
+
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_hal::prelude::*;
+
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -6,4 +13,14 @@ fn main() {
     esp_idf_sys::link_patches();
 
     println!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = peripherals.pins.gpio2.into_output().unwrap();
+
+    loop {
+        led.set_high().unwrap();
+        thread::sleep(Duration::from_millis(500));
+        led.set_low().unwrap();
+        thread::sleep(Duration::from_millis(500));
+    }
 }
