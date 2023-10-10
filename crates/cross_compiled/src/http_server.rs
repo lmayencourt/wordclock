5d361d577b1df5e08c2eb82f88e96a9a7ac12c39
Louis Mayencourt <louis.mayencourt@gmail.com>
2023-10-10 17:42:57 +0200

    Add CSS style to config html page

--- crates/cross_compiled/src/http_server.rs
+++ crates/cross_compiled/src/http_server.rs
@@ -55,7 +55,7 @@ fn home_page_handler(req: Request<&mut EspHttpConnection>) -> embedded_svc::http
     
     info!("Processing '/' request");
     let mut response = req.into_response(200, None, headers.as_slice())?;
-    response.write_all(config_form("hello wordclock!").as_bytes())?;
+    response.write_all(config_form("WordClock configuration").as_bytes())?;
 
     Ok(())
 }
@@ -85,33 +85,98 @@ fn get_handler(mut req: Request<&mut EspHttpConnection>) -> embedded_svc::http::
 fn config_form(content: impl AsRef<str>) -> String {
     format!(
         r##"
-<!DOCTYPE HTML>
-<html>
-    <head>
-    <title>Word-Clock</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-    <h1>{}</h1>
-    <form action="/get" target="hidden-form">
-        Wifi SSID (name): <input type="text" name="input_wifi_ssid"><br><br>
-        Wifi password: <input type="text" name="input_wifi_password"><br><br>
-        Night-mode starts at: <input type="text" name="input_night_mode_start">
-        <div class="tooltip">&#9432;
-            <span class="tooltiptext">E.g. 12:00 <br>
-            Must be between 12:00 and 23:59</span>
-        </div><br><br>
-        Night-mode ends at: <input type="text" name="input_night_mode_end">
-        <div class="tooltip">&#9432;
-            <span class="tooltiptext">E.g. 08:00 <br>
-            Must be between 00:00 and 12:00</span>
-        </div><br><br>
-        Display color: <input type="color" id="favcolor" name="favcolor" value="#ffffff"><br><br>
-        <input type="submit" value="Submit" onclick="submitMessage()">
-    </form><br>
-    <iframe style="display:none" name="hidden-form"></iframe>
-    </body>
-</html>
+        <!DOCTYPE HTML>
+        <html>
+            <head>
+            <title>Word-Clock</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <style>
+                * {{
+                    box-sizing: border-box;
+                    font-family: Arial, Helvetica, sans-serif;
+                }}
+                body {{
+                    color: #2c2c2c;
+                    margin: 32px;
+                }}
+                .config-card {{
+                    border: #ccd5ff solid;
+                    border-radius: 16px;
+                    margin-bottom: 16px;
+                    padding: 8px;
+                }}
+                h2.config-title {{
+                    margin-top: 2px;
+                    margin-bottom: 16px;
+                }}
+                .config-element {{
+                    padding: 8px;
+                    border-radius: 16px;
+                }}
+                input[type=text] {{
+                    border-radius: 16px;
+                    width: auto;
+                    margin-top: 4px;
+                }}
+                .config-element>label {{
+                    position:absolute;
+                    margin-top: -16px;
+                    text-align:center;
+                    vertical-align:bottom;
+                    font-style: normal;
+                    font-weight: 400;
+                    padding-top:2px;
+                }}
+                input[type=submit] {{
+                    background-color: #1755e6;
+                    color: white;
+                    padding: 10px;
+                    padding-left: 24px;
+                    padding-right: 24px;
+                    border: none;
+                    border-radius: 24px;
+                }}
+                input[type=submit]:hover {{
+                    background-color: #0a3494;
+                }}
+            </style>
+            <body>
+            <h1>{}</h1>
+            <form action="/get" target="hidden-form">
+                <div class="config-card">
+                    <h2 class="config-title">WiFi</h2>
+                    <div class="config-element">
+                        <label for="input_wifi_ssid">SSID (name)</label>
+                        <input type="text" class="form-control" name="input_wifi_ssid" placeholder="Your WiFi network name">
+                    </div>
+                    <div class="config-element">
+                        <label for="input_wifi_password">Password</label>
+                        <input type="text" name="input_wifi_password" placeholder="Your WiFi network password">
+                    </div>
+                </div>
+                <div class="config-card">
+                    <h2 class="config-title">Night-mode</h2>
+                    <div class="config-element">
+                        <label for="input_night_mode_start">Start at:</label>
+                        <input type="text" name="input_night_mode_start" placeholder="22:00">
+                        Must be between 12:00 and 23:59 
+                    </div>
+                    <div class="config-element">
+                        <label for="input_night_mode_end">End at:</label>
+                        <input type="text" name="input_night_mode_end" placeholder="06:30">
+                        Must be between 00:00 and 12:00
+                    </div>
+                </div>
+                <div class="config-card">
+                    <h2 class="config-title">Display color</h2>
+                    <input type="color" id="favcolor" name="favcolor" value="#ffffff">
+                </div>
+                <input id="submit" type="submit" value="Submit" onclick="submitMessage()">
+            </form>
+            <iframe style="display:none" name="hidden-form"></iframe>
+            </body>
+        </html>
 "##,
         content.as_ref()
     )
